fb9eae13ed38f0ade382f8205778a184daf37ae1
danawacomputer <taein2370@gmail.com>
2019-11-03 11:00:00 +0900

    first commit

---
+++ app/src/main/java/com/icecream/chat/MainActivity.java
@@ -0,0 +1,14 @@
+package com.icecream.chat;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.icecream.chat.R;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
