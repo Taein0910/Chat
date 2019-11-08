b45318280c02576df731762fa041acb2f0bfc968
IceCream0910 <44636839+IceCream0910@users.noreply.github.com>
2019-11-08 22:55:13 +0900

    chatFix

---
+++ app/src/main/java/com/icecream/chat/Fragments/ChatsFragment.java
@@ -0,0 +1,25 @@
+package com.icecream.chat.Fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.icecream.chat.R;
+
+
+public class ChatsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chats, container, false);
+    }
+
+}
